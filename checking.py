f90c84c201cdc5927cf6f5522f341187f490d9f4
abcdefghijkaren <kailin955@cycu.org.tw>
2025-07-13 14:28:56 +0800

    reruirements renew

--- checking.py
+++ checking.py
@@ -1,12 +1,10 @@
-# import pkg_resources
-# # 獲取所有已安裝的套件
-# installed_packages = pkg_resources.working_set
-# # 將套件名稱和版本格式化為 requirements.txt 格式
-# requirements = [f"{package.project_name}=={package.version}" for package in installed_packages]
-# # 將結果寫入 requirements.txt 檔案
-# with open("requirements.txt", "w") as f:
-#     for requirement in requirements:
-#         f.write(requirement + "\n")
-# print("requirements.txt 已生成，包含所有已安裝的套件及其版本。")
-import psycopg2
-print(psycopg2.__version__)
+import pkg_resources
+# 獲取所有已安裝的套件
+installed_packages = pkg_resources.working_set
+# 將套件名稱和版本格式化為 requirements.txt 格式
+requirements = [f"{package.project_name}=={package.version}" for package in installed_packages]
+# 將結果寫入 requirements.txt 檔案
+with open("requirements.txt", "w") as f:
+    for requirement in requirements:
+        f.write(requirement + "\n")
+print("requirements.txt 已生成，包含所有已安裝的套件及其版本。")
